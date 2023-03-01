802c6d30c08605839ac93857824e96817c66e5eb
Mariya Chernuhina <mariya.chernuhina@mail.ru>
2023-03-01 10:55:53 +0300

    add dataPicker

--- HabitViewCreate.swift
+++ HabitViewCreate.swift
@@ -30,7 +30,6 @@ class HabitViewCreate: UIView, UITextFieldDelegate {
     lazy var nameOfHabitTextField: UITextField = {
         let tfHabit = UITextField()
 
-
         tfHabit.font = .systemFont(ofSize: 17, weight: .regular)
         tfHabit.textColor = .black
 
@@ -73,6 +72,51 @@ class HabitViewCreate: UIView, UITextFieldDelegate {
 
     }()
 
+    private let timeTitle: UILabel = {
+        let titleTime = UILabel()
+        titleTime.text = "ВРЕМЯ"
+        titleTime.font = .systemFont(ofSize: 13, weight: .semibold)
+        titleTime.textColor = .black
+        titleTime.translatesAutoresizingMaskIntoConstraints = false
+
+        return titleTime
+    }()
+
+    private let everyDayLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Каждый день в"
+        label.font = .systemFont(ofSize: 17, weight: .regular)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        return label
+    }()
+
+    lazy var dateTextField: UITextField = {
+        let dateTF = UITextField()
+
+        dateTF.font = .systemFont(ofSize: 17, weight: .regular)
+        dateTF.textColor = UIColor(red: 161/255.0, green: 22/255.0, blue: 204/255.0, alpha: 1.0)
+        dateTF.textAlignment = .left
+        dateTF.autocapitalizationType = .none
+        dateTF.text = "00"
+        dateTF.translatesAutoresizingMaskIntoConstraints = false
+
+//        tfHabit.tintColor = .systemBlue
+
+        return  dateTF
+
+    }()
+
+    private let datePicker: UIDatePicker = {
+        let datePicker = UIDatePicker()
+        datePicker.frame = CGRect(x: 10, y: 50, width: 100, height: 200)
+        datePicker.timeZone = NSTimeZone.local
+                datePicker.backgroundColor = UIColor.white
+
+        return datePicker
+    }()
+
     let scrollView: UIScrollView = {
         var logScrollView = UIScrollView()
         logScrollView.keyboardDismissMode = .interactive
@@ -97,6 +141,7 @@ class HabitViewCreate: UIView, UITextFieldDelegate {
 
         addElements()
         addConstraints()
+        dateTextField.inputView = datePicker
 
     }
 
@@ -111,6 +156,10 @@ class HabitViewCreate: UIView, UITextFieldDelegate {
         contentView.addSubview(nameOfHabitTextField)
         contentView.addSubview(colorLabelName)
         contentView.addSubview(imageColor)
+        contentView.addSubview(timeTitle)
+        contentView.addSubview(everyDayLabel)
+
+        contentView.addSubview(dateTextField)
         imageColor.addGestureRecognizer(tapGestureRecognizer)
         tapGestureRecognizer.addTarget(self, action: #selector(colorTapGesture))
 
@@ -132,23 +181,40 @@ class HabitViewCreate: UIView, UITextFieldDelegate {
             titleLabelName.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
             titleLabelName.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15),
             titleLabelName.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15),
-//            titleLabelName.heightAnchor.constraint(equalToConstant: 50),
+            titleLabelName.heightAnchor.constraint(equalToConstant: 30),
 
             nameOfHabitTextField.topAnchor.constraint(equalTo: titleLabelName.bottomAnchor, constant: 10),
             nameOfHabitTextField.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15),
             nameOfHabitTextField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15),
+            nameOfHabitTextField.heightAnchor.constraint(equalToConstant: 30),
 
             colorLabelName.topAnchor.constraint(equalTo: nameOfHabitTextField.bottomAnchor, constant: 20),
             colorLabelName.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15),
             colorLabelName.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15),
+            colorLabelName.heightAnchor.constraint(equalToConstant: 30),
 
             imageColor.topAnchor.constraint(equalTo: colorLabelName.bottomAnchor, constant: 10),
             imageColor.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15),
 //            imageColor.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15),
             imageColor.heightAnchor.constraint(equalToConstant: 30),
             imageColor.widthAnchor.constraint(equalToConstant: 30),
-            imageColor.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+            imageColor.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
 
+            timeTitle.topAnchor.constraint(equalTo: imageColor.bottomAnchor, constant: 20),
+            timeTitle.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15),
+            timeTitle.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15),
+            timeTitle.heightAnchor.constraint(equalToConstant: 30),
+
+
+            everyDayLabel.topAnchor.constraint(equalTo: timeTitle.bottomAnchor, constant: 20),
+            everyDayLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15),
+            everyDayLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15),
+            everyDayLabel.heightAnchor.constraint(equalToConstant: 20),
+
+            dateTextField.topAnchor.constraint(equalTo: timeTitle.bottomAnchor, constant: 20),
+            dateTextField.leadingAnchor.constraint(equalTo: everyDayLabel.leadingAnchor, constant: 130),
+            dateTextField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15),
+            dateTextField.heightAnchor.constraint(equalToConstant: 20)
 
 
 
